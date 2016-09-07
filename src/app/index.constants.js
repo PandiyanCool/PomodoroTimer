725be449cea25ed55acefae446c8a2bd9cd1f24e
Pandiyan <pmurugan@burning-glass.com>
2016-09-07 18:45:36 +0530

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('pomodoroTimer')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
